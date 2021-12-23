08aff6542e961facbfde2946a9f8ecc38bf0d5ab
Luca Magnani <lucamagnani@digitalmens.it>
2021-12-23 15:14:52 +0100

    implemento excel db base + creazione url di ricerca

--- subito_searcher/subito-searcher.py
+++ subito_searcher/subito-searcher.py
@@ -9,45 +9,135 @@ import platform
 import re
 import time
 
+from openpyxl import load_workbook, Workbook
+
 from replit_keep_alive import replit_keep_alive
 
+
+#todo FIX workbook opened and not closed in methods
+
+global_excel_db = 'not_ready.xlsx'
+
+
+def fill_search_url_in_excel_db():
+    global min_price, max_price
+    # example1='https://www.subito.it/annunci-italia/vendita/arredamento-casalinghi/?q=herman+miller&order=priceasc&ps=100&pe=350'
+    url_root = 'https://www.subito.it/annunci-'
+    search_area_dic = {
+        'italia': 'italia',
+        'EM': 'emilia-romagna',
+        'EM vicino': 'emilia-romagna-vicino',
+        'RE': '/reggio-emilia',
+        'MO': '/modena',
+        'PR': '/parma'
+    }
+    categoria_dic = {
+        'Arredamento': 'arredamento-casalinghi',
+        'Informatica': 'informatica'
+    }
+    search_area = ws.cell(row=ws_row_number, column=col_names_number['SearchArea']).value
+    ##because provinces need region area and province sub areas in URL
+    if search_area in ['RE', 'MO', 'PR']:
+        sub_search_area = search_area_dic[search_area]
+        search_area = search_area_dic['EM']
+
+    else:
+        search_area = search_area_dic[search_area]
+        sub_search_area = ''
+    category = ws.cell(row=ws_row_number, column=col_names_number['Category']).value
+    category = categoria_dic[category]
+    keywords = ws.cell(row=ws_row_number, column=col_names_number['Keywords']).value
+    keywords = 'Herman Miller Sedia'
+    keywords = '+'.join([x.strip().lower() for x in keywords.split(' ')])
+    print()
+    min_price = ws.cell(row=ws_row_number, column=col_names_number['MinPrice']).value or None
+    max_price = ws.cell(row=ws_row_number, column=col_names_number['MaxPrice']).value or None
+    only_in_title = True if ws.cell(row=ws_row_number, column=col_names_number['Only in Title']).value else False
+    only_can_post = True if ws.cell(row=ws_row_number, column=col_names_number['Only Can Post']).value else False
+    search_url = url_root + search_area + '/vendita/' + category + sub_search_area + '/?q=' + keywords + '&ps=' + str(
+        min_price) + '&pe=' + str(max_price)
+    ws.cell(row=ws_row_number, column=col_names_number['URL Search']).value = search_url
+    wb.save(filename=global_excel_db)
+
+
+
+#SET UP EXCEL DB
+try:
+    wb = load_workbook(filename=global_excel_db)
+except:
+    #create excel db if not existing
+    wb = Workbook()
+    sheet = wb.active
+    column_names = 'Active,Search Name,MinPrice,MaxPrice,SearchArea,Category,Keywords,KeywordsEclude,Only in Title,Only Can Post,Website,URL Search'.split(',')
+    for cl in range(len(column_names)):
+        sheet.cell(row=1, column=cl+1, value=column_names[cl])
+    wb.save(filename=global_excel_db)
+
+ws = wb.active
+
+## Create a dictionary of column names
+col_names_number={}
+current_val=1
+for cnm in ws.iter_cols(1, ws.max_column):
+    col_names_number[cnm[0].value] = current_val
+    current_val+=1
+url_row_number= col_names_number['URL Search']
+
+##find columns that have no url
+for ws_row_number in range(1, ws.max_row+1):
+    if not ws.cell(row=ws_row_number, column=url_row_number).value:
+        fill_search_url_in_excel_db()
+
+
+
+
+
+#KEEP SCRIPT RUNNING 24/7 WITH REPLIT
 replit_keep_alive.keep_alive()
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--add", dest='name', help="name of new tracking to be added")
-parser.add_argument("--url", help="url for your new tracking's search query")
-parser.add_argument("--delete", help="name of the search you want to delete")
-parser.add_argument('--refresh', '-r', dest='refresh', action='store_true', help="refresh search results once")
-parser.set_defaults(refresh=False)
-parser.add_argument('--daemon', '-d', dest='daemon', action='store_true', help="keep refreshing search results forever (default delay 120 seconds)")
-parser.set_defaults(daemon=False)
-parser.add_argument('--delay', dest='delay', help="delay for the daemon option (in seconds)")
-parser.set_defaults(delay=120)
-parser.add_argument('--list', dest='list', action='store_true', help="print a list of current trackings")
-parser.set_defaults(list=False)
-parser.add_argument('--short_list', dest='short_list', action='store_true', help="print a more compact list")
-parser.set_defaults(short_list=False)
-parser.add_argument('--tgoff', dest='tgoff', action='store_true', help="turn off telegram messages")
-parser.set_defaults(tgoff=False)
-parser.add_argument('--notifyoff', dest='win_notifyoff', action='store_true', help="turn off windows notifications")
-parser.set_defaults(win_notifyoff=False)
-parser.add_argument('--addtoken', dest='token', help="telegram setup: add bot API token")
-parser.add_argument('--addchatid', dest='chatid', help="telegram setup: add bot chat id")
-#my parsers
-parser.add_argument("--only_with_price", dest='only_with_price', help="show ads without price: default True")
-parser.set_defaults(only_with_price=True)
-parser.add_argument("--min_price", dest='min_price', help="min price for captured ads; default 0")
-parser.set_defaults(min_price=0)
-parser.add_argument("--max_price", dest='max_price', help="max price for captured ads; default 10.000")
-parser.set_defaults(min_price=10000)
-parser.add_argument("--location", dest='location', help="ads geographical location")
-parser.set_defaults(location='Reggio Emilia')
-parser.add_argument("--keyword_in_title", dest='keyword_in_title', help="show only ads with keyword in the title: default False")
-parser.set_defaults(keyword_in_title=False)
-parser.add_argument("--first_notify", dest='first_notify', help="notifies all results upon first search: default False")
-parser.set_defaults(first_notify=False)
+def parser_set_up():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--add", dest='name', help="name of new tracking to be added")
+    parser.add_argument("--url", help="url for your new tracking's search query")
+    parser.add_argument("--delete", help="name of the search you want to delete")
+    parser.add_argument('--refresh', '-r', dest='refresh', action='store_true', help="refresh search results once")
+    parser.set_defaults(refresh=False)
+    parser.add_argument('--daemon', '-d', dest='daemon', action='store_true',
+                        help="keep refreshing search results forever (default delay 120 seconds)")
+    parser.set_defaults(daemon=False)
+    parser.add_argument('--delay', dest='delay', help="delay for the daemon option (in seconds)")
+    parser.set_defaults(delay=120)
+    parser.add_argument('--list', dest='list', action='store_true', help="print a list of current trackings")
+    parser.set_defaults(list=False)
+    parser.add_argument('--short_list', dest='short_list', action='store_true', help="print a more compact list")
+    parser.set_defaults(short_list=False)
+    parser.add_argument('--tgoff', dest='tgoff', action='store_true', help="turn off telegram messages")
+    parser.set_defaults(tgoff=False)
+    parser.add_argument('--notifyoff', dest='win_notifyoff', action='store_true', help="turn off windows notifications")
+    parser.set_defaults(win_notifyoff=False)
+    parser.add_argument('--addtoken', dest='token', help="telegram setup: add bot API token")
+    parser.add_argument('--addchatid', dest='chatid', help="telegram setup: add bot chat id")
+    # my parsers
+    parser.add_argument("--only_with_price", dest='only_with_price', help="show ads without price: default True")
+    parser.set_defaults(only_with_price=True)
+    parser.add_argument("--min_price", dest='min_price', help="min price for captured ads; default 0")
+    parser.set_defaults(min_price=0)
+    parser.add_argument("--max_price", dest='max_price', help="max price for captured ads; default 10.000")
+    parser.set_defaults(min_price=10000)
+    parser.add_argument("--location", dest='location', help="ads geographical location")
+    parser.set_defaults(location='Reggio Emilia')
+    parser.add_argument("--keyword_in_title", dest='keyword_in_title',
+                        help="show only ads with keyword in the title: default False")
+    parser.set_defaults(keyword_in_title=False)
+    parser.add_argument("--first_notify", dest='first_notify',
+                        help="notifies all results upon first search: default False")
+    parser.set_defaults(first_notify=False)
+    return parser
 
+
+#PARSER SET UP
+parser = parser_set_up()
 
 args = parser.parse_args()
 
@@ -56,9 +146,9 @@ apiCredentials = dict()
 dbFile = "searches.tracked"
 telegramApiFile = "telegram_api_credentials"
 
-#other settings
-min_price = None
-max_price = None
+# #other settings
+# min_price = None
+# max_price = None
 
 # Windows notifications
 if platform.system() == "Windows":
