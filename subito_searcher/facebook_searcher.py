17081f2d881914beac127575a9a8c4daa3432c5b
Luca Magnani <lucamagnani@digitalmens.it>
2021-12-23 11:17:26 +0100

    b4 implementazione excel db

---
+++ subito_searcher/facebook_searcher.py
@@ -0,0 +1,68 @@
+import argparse
+import requests
+from bs4 import BeautifulSoup, Tag
+import json
+import os
+import platform
+import re
+import time
+
+url = 'https://www.facebook.com/marketplace/111926645500644/search/?minPrice=1000&maxPrice=6000&query=fiat&exact=false'
+
+
+def run_query(url, name, notify):
+    print("running query (\"{}\" - {})...".format(name, url))
+    global queries
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    print(soup.prettify())
+
+    product_list_items = soup.find_all('div', class_=re.compile(r'item-key-data'))
+    msg = []
+
+    for product in product_list_items:
+        title = product.find('h2').string
+
+        try:
+            price =product.find('p' ,class_=re.compile(r'price')).contents[0]
+            # check if the span tag exists
+            price_soup = BeautifulSoup(price, 'html.parser')
+            if type(price_soup) == Tag:
+                continue
+            # at the moment (20.5.2021) the price is under the 'p' tag with 'span' inside if shipping available
+
+        except:
+            price = "Unknown price"
+        link = product.parent.parent.parent.parent.get('href')
+
+        location = product.find('span' ,re.compile(r'town')).string + product.find('span' ,re.compile(r'city')).string
+
+
+        if not queries.get(name):   # insert the new search
+            queries[name] = {url: {link: {'title': title, 'price': price, 'location': location}}}
+            print("\nNew search added:", name)
+            print("Adding result:", title, "-", price, "-", location)
+        else:   # add search results to dictionary
+            if not queries.get(name).get(url).get(link):   # found a new element
+                tmp = "New element found for  " + name +":  " + title +" @  " + price +" -  " +location +" -->  " +link +'\n'
+                msg.append(tmp)
+                queries[name][url][link] = {'title': title, 'price': price, 'location': location}
+
+    # if len(msg) > 0:
+    #     if notify:
+    #         # Windows only: send notification
+    #         if not args.win_notifyoff and platform.system() == "Windows":
+    #             global toaster
+    #             toaster.show_toast("New announcements", "Query: " + name)
+    #         if is_telegram_active():
+    #             send_telegram_messages(msg)
+    #         print("\n".join(msg))
+    #         print('\n{} new elements have been found.'.format(len(msg)))
+    #     save_queries()
+    # else:
+    #     print('\nAll lists are already up to date.')
+    # print("queries file saved: ", queries)
+
+
+run_query(url, 'fiat', True)
